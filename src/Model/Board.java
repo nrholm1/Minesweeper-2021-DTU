56dc75aa98a98f890a0f5693d519898eb9bd8c8e
nrholm1 <nielsraunkjaer@gmail.com>
2021-01-07 11:07:36 +0100

    Add Model Classes

--- src/Model/Board.java
+++ src/Model/Board.java
@@ -1,4 +1,70 @@
 package Model;
 
 public class Board {
+    Field[][] minefield;
+
+    double probability;
+    int rowLength;
+    int colLength;
+
+
+    public Field getField(int r, int c) {
+        return minefield[r][c];
+    }
+
+    public void setFieldState(int r, int c) {
+        // set field state when pressed
+    }
+
+    public int getRowLength() {
+        return rowLength;
+    }
+
+    public int getColLength() {
+        return colLength;
+    }
+
+    public Board(double _probability, int _rowLength, int _colLength) {
+        this.probability = _probability;
+        this.rowLength = _rowLength;
+        this.colLength = _colLength;
+
+        makeMinefieldWithDimensions();
+    }
+
+    public void makeMinefieldWithDimensions() {
+        minefield = new Field[this.colLength][this.rowLength];
+
+        for(int row = 0; row < minefield.length; row++)
+            for(int col = 0; col < minefield[row].length; col++) {
+                boolean _isMine = (Math.random() < this.probability);
+                minefield[row][col] = new Field(_isMine);
+            }
+
+        setAdjacentMineCounters();
+    }
+
+    private void setAdjacentMineCounters() {
+        for(int c = 0; c < this.colLength; c++)
+            for(int r = 0; r < this.rowLength; r++)
+                if (minefield[c][r].isMine())
+                    incrementAdjacentFieldsAdjacentMineCounter(c, r);
+    }
+
+    private void incrementAdjacentFieldsAdjacentMineCounter(int col, int row) {
+        for(int c = -1; c <= 1; c++)
+            for(int r = -1; r <= 1; r++)
+                if (col + c >= 0 && col + c < this.colLength &&
+                        row + r >= 0 && row + r < this.rowLength &&
+                        (c != 0 || r != 0))
+                    minefield[col + c][row + r].incrementAdjacentMines();
+    }
+
+    public void printMinefield() {
+        for(Field[] row : this.minefield) {
+            for(Field i : row)
+                System.out.print(i + " ");
+            System.out.println();
+        }
+    }
 }
--- src/Model/Field.java
+++ src/Model/Field.java
@@ -1,4 +1,57 @@
 package Model;
 
 public class Field {
+    private State state;
+    private boolean isMine;
+    private int adjacentMines;
+
+    public Field(boolean _isMine) {
+        this.state = State.UNFLAGGED;
+        this.isMine = _isMine;
+        this.adjacentMines = 0;
+    }
+
+    public State getState() {
+        return this.state;
+    }
+
+    public void setState(State _state) {
+        this.state = _state;
+    }
+
+    public boolean isMine() {
+        return this.isMine;
+    }
+
+    public void toggleIsMine() {
+        this.isMine = !this.isMine;
+    }
+
+    public int getAdjacentMines() {
+        return this.adjacentMines;
+    }
+
+    public void setAdjacentMines(int _adjacentMines) {
+        this.adjacentMines = _adjacentMines;
+    }
+
+    public void incrementAdjacentMines() {
+        this.adjacentMines++;
+    }
+
+    public String toString() {
+        String retval = "";
+
+        retval += this.isMine ?
+                "X" : (this.adjacentMines > 0 ?
+                    this.adjacentMines : " ");
+
+        return retval;
+    }
+
+    public enum State {
+        FLAGGED,
+        UNFLAGGED,
+        PRESSED
+    }
 }
