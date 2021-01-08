8d9fde84c2e5556a528433b3124f2c300b0928cf
BuildTools <unconfigured@null.spigotmc.org>
2021-01-08 14:56:42 +0100

    Made Framework for Integration

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,8 @@
-import View.MainMenu;
 import javafx.application.Application;
 import javafx.scene.Scene;
-import javafx.scene.control.Button;
-import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
 
-public class JavaFxTest extends Application {
+public class Main extends Application {
     public static void main(String[] args) {
         launch(args);
     }
@@ -14,10 +11,6 @@ public class JavaFxTest extends Application {
     public void start(Stage primaryStage) {
         Stage window = primaryStage;
         window.setTitle("MineSwoop");
-
-        Scene pregame = new MainMenu(new int[]{1000, 600}).getScene();
-
-        window.setScene(pregame);
         window.show();
     }
 }
