7c20ca7a7d3ad91b6e9a2cd7c919cda94ebbe6b9
Massimo Angelo Hansen <massimo.hansen@gmail.com>
2021-01-15 11:50:59 +0100

    victory back

--- src/View/VictoryScreen/VictoryView.java
+++ src/View/VictoryScreen/VictoryView.java
@@ -24,10 +24,10 @@ public class VictoryView extends Scene {
         congratulations = new Text("Congratulations!");
         setTextType(congratulations, stagedims[0]/30);
 
-        extra = new Text("You've cleared all the mines,\n so now the submarine can pass...");
+        extra = new Text("You've cleared all the mines, and now the submarine can pass...");
         setTextType(extra, stagedims[0]/60);
 
-        time = new Text("You did it in: \n" + seconds + "seconds");
+        time = new Text("You did it in " + seconds + " seconds");
         setTextType(extra, stagedims[0]/60);
 
         next = new PixelButton(ExternalResources.topmenuNewGame, stagedims[0]/6, stagedims[0]/18);
@@ -37,6 +37,7 @@ public class VictoryView extends Scene {
         content.getChildren().addAll(congratulations,extra,time,next);
 
         whole = new StackPane();
+        whole.setId("whole");
         whole.setStyle("-fx-background-color: #306082");
         whole.getChildren().addAll(content);
         super.setRoot(whole);
