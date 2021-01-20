06ae66841c9d0870b4a2df6f09e84ed1cf24fdb1
BuildTools <unconfigured@null.spigotmc.org>
2021-01-20 09:41:49 +0100

    Changed Math

--- src/Model/Util/BoardBuilder.java
+++ src/Model/Util/BoardBuilder.java
@@ -30,7 +30,7 @@ public class BoardBuilder {
     //difficulty goes from 1 - 10. min is 4% mines, max is 20% mines
     public BoardBuilder withDifficulty(int diff) {
         int amountField = 6 * ((radius + 1)*(int)(radius/2) + (radius % 2 == 0? 0 : (radius + 1)/2)) + 1; // Calculates amount of fields from size
-        double percent =  ((16.0/9.0 * (double)diff + (20.0-16.0/9.0*10.0)) / 100.0);
+        double percent =  ((16.0/9.0 * (double)diff + 20.0/9.0) / 100.0);
         this.amountMines = (int)(amountField * percent);
         return this;
     }
