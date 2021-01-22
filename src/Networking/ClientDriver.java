c63e3b5de133e2d43089ed9ff4699812d8c4e428
Niels Raunkjær <nielsraunkjaer@gmail.com>
2021-01-22 10:16:33 +0100

    Write explanatory comments in Networking module, refactor ClientDriver

--- src/Networking/ClientDriver.java
+++ src/Networking/ClientDriver.java
@@ -5,13 +5,21 @@ import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
 
+// s204503 - Niels Raunkjær Holm
+// This class builds and sends an http POST request from the target IP and sets the FieldDTO as payload.
+// An HttpClient is created and an HttpRequest is built using the HttpRequest.Builder class.
+// The format of the http request is URI: "http://{IP}:{PORT}/swoop" and the payload is a
+// serialized plaintext representation of the given FieldDTO object.
+// The client then sends a non-blocking request using the sendAsync method (note: is mostly asynchronous)
+
 public abstract class ClientDriver {
-
-
     // ASYNC
     // chained methods after sendAsync makes it work async
+    static HttpClient client;
+
     public static void simpleAsyncRequestPrintFieldDTO(String IP, int PORT, FieldDTO fieldDTO) {
-        HttpClient client = HttpClient.newBuilder().build();
+        if (client == null)
+            client = HttpClient.newBuilder().build();
         HttpRequest request = HttpRequest.newBuilder()
                 .uri(URI.create("http://" + IP + ":" + PORT + "/swoop"))
                 .header("Content-Type", "text/plain")
--- src/Networking/HttpListener.java
+++ src/Networking/HttpListener.java
@@ -6,6 +6,16 @@ import com.sun.net.httpserver.HttpHandler;
 import java.io.IOException;
 import java.io.OutputStream;
 
+// s204503 - Niels Raunkjær Holm
+// This class is the 'server' part of the networking module.
+// It implements the HttpHandler interface from the com.sun.net.httpserver package.
+// HttpListener runs on its own thread and simply listens for incoming requests.
+// When it receives a request, it reads the bytes and passes it to the parser for
+// parsing the FieldDTO object contained in the payload.
+// The DTO is then handled further in MultiplayerService.
+// Lastly, a http response is built throughout this, containing the elapsed time
+// and the 200 OK response code.
+
 public class HttpListener implements HttpHandler {
     @Override
     public void handle(HttpExchange exchange) throws IOException {
